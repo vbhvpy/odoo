17c11f231cbc58bdbeed4a7e48be6ad60abe06c8
vbhvpy <121506605+vbhvpy@users.noreply.github.com>
2023-01-30 14:08:22 +0400

    Add files via upload

---
+++ performance_evaluation/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Perfomance Evaluation",
+
+    'summary': """
+         Extend functioality for performance Evaluation""",
+
+    'description': """
+        This will be used for the Employee performance evaluation Program in the system.
+    """,
+
+    'author': "Snackat Cafe",
+    'website': "http://www.snackatcafe.com",
+
+    'category': 'Human Resources/Employees',
+    'version': '14.0.1',
+    'license': 'AGPL-3',
+    'depends': ['hr', 'base'],
+    'data': [
+        'security/performance_evaluation_security.xml',
+        'security/ir.model.access.csv',
+        'views/performance_evaluation_view.xml',
+        'data/ir_sequence_data.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+        
+}
